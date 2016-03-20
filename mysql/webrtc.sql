c887ab625a93a4ac719d794f2cdcbb7f9242d040
Szabolcs Bene <bene_szabolcs@yahoo.com>
2016-03-20 21:09:24 +0200

    update db

--- mysql/webrtc.sql
+++ mysql/webrtc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.0.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 15, 2016 at 11:17 PM
--- Server version: 10.0.17-MariaDB
--- PHP Version: 5.6.14
+-- Generation Time: Mar 20, 2016 at 08:08 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,6 +30,13 @@ CREATE TABLE `friends` (
   `userid` int(11) NOT NULL,
   `friendids` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`userid`, `friendids`) VALUES
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -56,6 +63,14 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `mail`) VALUES
+(1, 'bene', '1234', 'mbsz3@yahoo.com'),
+(2, 'szabi', '1234', 'mbsz2@yahoo.com');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -75,7 +90,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
