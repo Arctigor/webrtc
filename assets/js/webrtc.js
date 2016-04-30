3ef5c1ef244272f5187af3eb0562904aa7b1c9db
Bene Lori <test@test.com>
2016-04-30 16:07:31 +0300

    create friends table

--- assets/js/webrtc.js
+++ assets/js/webrtc.js
@@ -6,126 +6,143 @@ function webRTC() {
 	var localPeerConnection = null; // WebRTC PeerConnection
 	var dataChannel = null; // WebRTC DataChannel
 	var constraints = {
-			'mandatory' :
-			{
-				'OfferToReceiveAudio' : true,
-				'OfferToReceiveVideo' : true,
-				'iceTransports': 'relay'
-			}
-
+		'mandatory' : {
+			'OfferToReceiveAudio' : true,
+			'OfferToReceiveVideo' : true,
+			'iceTransports' : 'relay'
 		}
+
+	}
 	var startButton = document.getElementById('startButton');
 	var offerJSON;
-	
+
 	RTCPeerConnection = webkitRTCPeerConnection;
 	RTCIceCandidate = window.RTCIceCandidate;
 	RTCSessionDescription = window.RTCSessionDescription;
-	
+
 	startButton.onclick = createConnection;
-	
+
 	var checkOffer = 5000;
- 	setInterval(waitingForOffer, checkOffer); 
-	
-	function createConnection(){
+	createFriendsTable();
+	setInterval(waitingForOffer, checkOffer);
+
+	function createConnection() {
 		localPeerConnection = new RTCPeerConnection(null);
 		localPeerConnection.createOffer(createOffer, errorHandler);
 	}
-	
-	function createOffer(sessionDescriptionProtocol){
+
+	function createOffer(sessionDescriptionProtocol) {
 		localPeerConnection = new RTCPeerConnection(null);
-		localPeerConnection.createOffer(function(sessionDescriptionProtocol) {
-			localPeerConnection.setLocalDescription(sessionDescriptionProtocol);
-			var myId = getMyId();
-			var peerId = getPeerId();
-			offerJSON = {
-					myId:myId,
-					peerId:peerId,
-					type:"offer",
-					data:sessionDescriptionProtocol.sdp
-			};
-			insertDataToDb(offerJSON, "/insertOffer");
-			offerer = true;
-		}, errorHandler);
+		localPeerConnection.createOffer(
+				function(sessionDescriptionProtocol) {
+					localPeerConnection
+							.setLocalDescription(sessionDescriptionProtocol);
+					var myId = getMyId();
+					var peerId = getPeerId();
+					offerJSON = {
+						myId : myId,
+						peerId : peerId,
+						type : "offer",
+						data : sessionDescriptionProtocol.sdp
+					};
+					insertDataToDb(offerJSON, "/insertOffer");
+					offerer = true;
+				}, errorHandler);
 	}
-	
-	function createAnswer(responseJSON){
+
+	function createAnswer(responseJSON) {
 		var remoteSDP = new RTCSessionDescription();
 		remoteSDP.type = "offer";
 		remoteSDP.sdp = responseJSON.offerersdp;
-		
+
 		localPeerConnection = new RTCPeerConnection(null);
 		localPeerConnection.setRemoteDescription(remoteSDP);
-		localPeerConnection.createAnswer(function(sessionDescriptionProtocol) {
-			localPeerConnection.setLocalDescription(sessionDescriptionProtocol);
-			var myId = getMyId();
-			var peerId = getPeerId();
-			answerJSON = {
-					myId:myId,
-					peerId:peerId,
-					type:"answer",
-					data:sessionDescriptionProtocol.sdp
-			};
-			insertDataToDb(answerJSON, "/insertAnswer");
-			}, errorHandler);
+		localPeerConnection.createAnswer(
+				function(sessionDescriptionProtocol) {
+					localPeerConnection
+							.setLocalDescription(sessionDescriptionProtocol);
+					var myId = getMyId();
+					var peerId = getPeerId();
+					answerJSON = {
+						myId : myId,
+						peerId : peerId,
+						type : "answer",
+						data : sessionDescriptionProtocol.sdp
+					};
+					insertDataToDb(answerJSON, "/insertAnswer");
+				}, errorHandler);
 	}
-	
-	function insertDataToDb(offerJSON, url){
+
+	function insertDataToDb(offerJSON, url) {
 		$.ajax({
-	         data: offerJSON,
-	         type: "post",
-	         url: url,
+			data : offerJSON,
+			type : "post",
+			url : url,
 		});
 	}
-	
-	function waitingForOffer(){
-		if(!answeredConnection){
+
+	function waitingForOffer() {
+		if (!answeredConnection) {
 			var success = null;
-			if(!offerer){
+			if (!offerer) {
 				var responseJSON = getOfferFromDb();
-				if(responseJSON != null){
+				if (responseJSON != null) {
 					createAnswer(responseJSON);
 					answeredConnection = true;
 				}
 			}
 		}
 	}
-	
-	function getOfferFromDb(){
+
+	function getOfferFromDb() {
 		var myId = getMyId();
 		var responseJSON = null;
 		myDataJSON = {
-				myId:myId
-			};
+			myId : myId
+		};
 		$.ajax({
-			 data: myDataJSON,
-	         type: "post",
-	         url: "/getOffer",
-	         async:false,
-	         success:function(response){ 
-	        	 responseJSON = response;
-	            }
+			data : myDataJSON,
+			type : "post",
+			url : "/getOffer",
+			async : false,
+			success : function(response) {
+				responseJSON = response;
+			}
 		});
 		return responseJSON;
 	}
-	
-	function errorHandler(error){
+
+	function errorHandler(error) {
 		console.error("Error at create offer: " + error);
 	}
+
+	function createFriendsTable() {
+		var friendsList = ["a","b","c"];
+		populateTable(friendsList);
+	}
 	
-	function getMyId(){
-		console.log(getCookie('id'));
+	function populateTable(friendsList){
+		var friendsTable = document.getElementById("friendsTable");
+		$.each(friendsList, function( key, value ) {
+			var row = friendsTable.insertRow(-1);
+			var cell = row.insertCell(0);
+			cell.innerHTML = value;
+			});
+	}
+
+	function getMyId() {
 		return getCookie('id');
 	}
-	
-	
-	function getPeerId(){
+
+	function getPeerId() {
 		return 2;
 	}
-	
+
 	function getCookie(name) {
 		var value = "; " + document.cookie;
 		var parts = value.split("; " + name + "=");
-		if (parts.length == 2) return parts.pop().split(";").shift();
+		if (parts.length == 2)
+			return parts.pop().split(";").shift();
 	}
 
 }
--- templates/welcome.php
+++ templates/welcome.php
@@ -1,21 +1,28 @@
 <div class="content">
-	Welcome <span id="username"><?php print $_SESSION['username']; ?></span>
-			<div id="buttons">
-				<button id="startButton">Start</button>
-				<button id="sendButton">Send</button>
-				<button id="closeButton">Stop</button>
-			</div>
+	Welcome <span id="username"><?php print $_SESSION['username']; ?>
+	</span>
+	<div id="buttons">
+		<button id="startButton">Start</button>
+		<button id="sendButton">Send</button>
+		<button id="closeButton">Stop</button>
+	</div>
 
-			<div id="sendReceive">
-				<div id="send">
-					<h2>Send</h2>
-					<textarea id="dataChannelSend" disabled
-						placeholder="Press Start, enter some text, then press Send."></textarea>
-				</div>
-				<div id="receive">
-					<h2>Receive</h2>
-					<textarea id="dataChannelReceive" disabled></textarea>
-				</div>
-			</div>
+	<div id="sendReceive">
+		<div id="send">
+			<h2>Send</h2>
+			<textarea id="dataChannelSend" disabled
+				placeholder="Press Start, enter some text, then press Send."></textarea>
+		</div>
+		<div id="receive">
+			<h2>Receive</h2>
+			<textarea id="dataChannelReceive" disabled></textarea>
+		</div>
+	</div>
+	<br></br>
+	<table id="friendsTable"class="tg">
+		<tr>
+			<th class="tg-yw4l">Friends</th>
+		</tr>
+	</table>
 
 </div>
