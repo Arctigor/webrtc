9fe643e536715e5a1579fb4945aa7124b46dc736
Szabolcs Bene <bene_szabolcs@yahoo.com>
2016-06-12 12:02:24 +0300

    add css to register

--- templates/home.php
+++ templates/home.php
@@ -1,8 +1,11 @@
-<div class="content">
+<link rel="stylesheet" type="text/css" href="assets/css/header.css">
+<div>
+<label class="successMessage">
 	<?php 	
 		if(isset($_SESSION['success']) && $_SESSION['success'] != ""){
 			print_r($_SESSION['success']);
 			$_SESSION['success'] = "";
 		}
 	?>
+</label>
 </div>
--- templates/register.php
+++ templates/register.php
@@ -1,19 +1,22 @@
-<div class="content">
-  <form action="formRegister" method="POST">
-  Username<br>
-  <input type="text" name="username" placeholder="Username"><br>
-  Email<br>
-  <input type="text" name="email" placeholder="emailname@example.com"><br>
-  Password<br>
-  <input type="password" name="password" placeholder="Enter your password"><br>
-  Confirm Password<br>
-  <input type="password" name="confirm-password" placeholder="Enter your password"><br><br>
-  <input type="submit" value="Register">
-</form>
+<link rel="stylesheet" type="text/css" href="assets/css/form.css">
+<div class="menu">
+	<form action="formRegister" method="POST">
+		<label> Username: </label> <input type="text" name="username"
+			class="inputBoxClass" placeholder="Username"><br> <br> <label> Email:
+		</label> <input type="text" name="email" class="inputBoxClass"
+			placeholder="emailname@example.com"><br> <br> <label>Password:</label>
+		<input type="password" name="password" class="inputBoxClass"
+			placeholder="Enter your password"><br> <br> <label>Confirm Password:</label>
+		<input type="password" name="confirm-password" class="inputBoxClass"
+			placeholder="Enter your password"><br> <br> <input type="submit"
+			class="submitButtonClass" value="Register">
+	</form>
+	<label class="errorMessage">
 	<?php 	
-		if(isset($_SESSION['error']) && $_SESSION['error'] != ""){
+	if(isset($_SESSION['error']) && $_SESSION['error'] != ""){
 			print_r($_SESSION['error']);
 			$_SESSION['error'] = "";
 		}
-	?>
+		?>
+	</label>
 </div>
