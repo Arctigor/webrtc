452bf38eced7687a5fa62dc12e0bd9e466919299
Szabolcs Bene <bene_szabolcs@yahoo.com>
2016-06-14 14:14:15 +0300

    fix menu items

--- templates/header.php
+++ templates/header.php
@@ -4,16 +4,33 @@
 <head>
 </head>
 <body>
-<link rel="stylesheet" href="/assets/css/header.css" />
+	<link rel="stylesheet" href="/assets/css/header.css" />
 	<div class="container">
+		<?php 	
+		if(!isset($_SESSION['username'])){
+	?>
 		<div>
 			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/login">Login</a></li>
-				<li><a href="/loginWithFb">Login with Fb</a></li>
-				<li><a href="/logout">Logout</a></li>
-				<li><a href="/register">Register</a></li>
+				<li><a href="/loginWithFb">Login with Fb</a>
+				</li>
+				<li><a href="/login">Login</a>
+				</li>
+				<li><a href="/register">Register</a>
+				</li>
+				<li><a href="/home">Home</a>
+				</li>
 			</ul>
 		</div>
+		<?php 
+		}else{
+	?>
+		<div>
+			<ul>
+				<li><a href="/logout">Logout</a>
+				</li>
+				<li><a href="/home">Home</a>
+				</li>
+			</ul>
+		</div>
+		<?php }	?>
 	</div>
-		
